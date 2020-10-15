d57c1507bc9df9654aa08a952bbc25a60b35e652
Santiago <santi.cardona.12@icloud.com>
2020-10-15 07:38:54 -0500

    Rediseño de inicio de sesión

--- src/login.java
+++ src/login.java
@@ -11,8 +11,8 @@ import java.awt.event.ActionEvent;
 
 public class login {
 
-	public JFrame frame;
-	private JTextField textField;
+	public JFrame frmInicioDeSesin;
+	private JTextField txtCorreo;
 	private JPasswordField passwordField;
 	public static boolean auth = false;
 	public static String buf = null;
@@ -25,7 +25,7 @@ public class login {
 			public void run() {
 				try {
 					login window = new login();
-					window.frame.setVisible(true);
+					window.frmInicioDeSesin.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,29 +44,31 @@ public class login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 354, 141);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmInicioDeSesin = new JFrame();
+		frmInicioDeSesin.setTitle("Inicio de sesi\u00F3n");
+		frmInicioDeSesin.setBounds(100, 100, 435, 163);
+		frmInicioDeSesin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmInicioDeSesin.getContentPane().setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Intoduzca sus credenciales");
-		lblNewLabel.setBounds(10, 11, 198, 14);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(10, 11, 136, 14);
+		frmInicioDeSesin.getContentPane().add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(10, 36, 198, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtCorreo = new JTextField();
+		txtCorreo.setToolTipText("Usuario");
+		txtCorreo.setBounds(10, 48, 198, 20);
+		frmInicioDeSesin.getContentPane().add(txtCorreo);
+		txtCorreo.setColumns(10);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(10, 67, 198, 20);
-		frame.getContentPane().add(passwordField);
+		passwordField.setBounds(10, 92, 198, 20);
+		frmInicioDeSesin.getContentPane().add(passwordField);
 		
 		JButton btnNewButton = new JButton("Entrar");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(textField.getText() != null && passwordField.getText() != null) {
-					buf = authenticator.connect(textField.getText(), AES.encrypt(passwordField.getText(), AES.keyString));
+				if(txtCorreo.getText() != null && passwordField.getText() != null) {
+					buf = authenticator.connect(txtCorreo.getText(), AES.encrypt(passwordField.getText(), AES.keyString));
 					if(buf != null && !buf.contains("E")) {
 						JOptionPane.showMessageDialog(null, "Se ha conectado exitosamente al servidor!" ,"Info", 1);
 						auth = true;
@@ -79,7 +81,23 @@ public class login {
 				}
 			}
 		});
-		btnNewButton.setBounds(239, 66, 89, 23);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(317, 91, 89, 23);
+		frmInicioDeSesin.getContentPane().add(btnNewButton);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		lblNewLabel_1.setBounds(10, 34, 46, 14);
+		frmInicioDeSesin.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Contrase\u00F1a");
+		lblNewLabel_2.setBounds(10, 78, 89, 14);
+		frmInicioDeSesin.getContentPane().add(lblNewLabel_2);
+		
+		JButton btnNewButton_1 = new JButton("Registro");
+		btnNewButton_1.setBounds(216, 91, 89, 23);
+		frmInicioDeSesin.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Olvid\u00E9 mi contrase\u00F1a");
+		btnNewButton_2.setBounds(216, 47, 190, 23);
+		frmInicioDeSesin.getContentPane().add(btnNewButton_2);
 	}
 }
